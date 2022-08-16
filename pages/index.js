a84f581d4b9b34c43d37aeb4c41fd3be9b724472
KML <kaungmyatlin.worklife@gmail.com>
2022-08-16 15:58:46 +0630

    rmDUMMY

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,6 @@
 import MeetupList from '../components/meetups/MeetupList'
 import { MongoClient } from 'mongodb'
 // import { useEffect } from 'react'
-const DUMMY = [
-    {
-        id: 'm1',
-        title: 'A first meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 5, 12345 Some City',
-        descritpion: 'This is a first meetup'
-    },
-    {
-        id: 'm2',
-        title: 'A second meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d7/Museum_Barberini_%26_Alter_Markt_in_Potsdam_2.jpg',
-        address: 'Some address 10, 12345 Some City',
-        descritpion: 'This is a second meetup'
-    },
-]
-
 const HomePage = (props) => {
     // below code no longer needed if we switch to 
     // data-fetching for static site generation.
